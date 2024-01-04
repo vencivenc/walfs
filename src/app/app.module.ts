a119f099ab6595a15bf2be1c7856f9e17492cf24
ventsislavkyorovski <ventsi@ship.cars>
2024-01-04 23:02:10 +0200

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './controller/app.controller';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: '/code/src/fe/build',
+      rootPath: join(__dirname, '..', 'build'),
     }),
   ],
   controllers: [AppController],
